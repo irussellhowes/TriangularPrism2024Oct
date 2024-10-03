a6d4e5ae9d49a0727f3a100d0ebc4936968876f1
Isla Russell-Howes <irussellhowes@lcs.on.ca>
2024-10-03 09:21:25 -0400

    Initial Commit

---
+++ TriangularPrism/TriangularPrismApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriangularPrismApp.swift
+//  TriangularPrism
+//
+//  Created by Isla Russell-Howes on 2024-10-03.
+//
+
+import SwiftUI
+
+@main
+struct TriangularPrismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriangularPrismTests/TriangularPrismTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriangularPrismTests.swift
+//  TriangularPrismTests
+//
+//  Created by Isla Russell-Howes on 2024-10-03.
+//
+
+import Testing
+@testable import TriangularPrism
+
+struct TriangularPrismTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
